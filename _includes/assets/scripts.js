5499b7297fd6a8589cc73ea7b7a69fc94639c15e
Alexander Stevenson <STEVAJ3@student.op.ac.nz>
2021-05-29 13:36:04 +1200

    Adding day names to dates

--- _includes/assets/scripts.js
+++ _includes/assets/scripts.js
@@ -25,7 +25,6 @@ fetch(url, {
 
 function changeTimeZone(time){      // Function changes API output to match that of the device it is on.
   let date = new Date(time);
-  // console.log(date.toLocaleString());
   return date.toLocaleString();
 }
 
@@ -36,11 +35,18 @@ function processTideData(data){     // Extractes the tides data form the fetch q
 
   for (let i = 0; i < tideValues.length; i++){        // for loop runs through all the data entries form the fetch and prints them out to article.
     const tideEntry = document.createElement('p');
+    const tideEntry2 = document.createElement('h3');
+    // const dayCheck = 7;
     if(tideValues[i].value < 1){      // if statment only prints them out if the value is less that 1 (aka low tide).
-      
-    tideEntry.textContent = (changeTimeZone(tideValues[i].time) + " | " + (tideValues[i].value));
+      let day = new Date(changeTimeZone(tideValues[i].time));
+      let dayName = weekdays[day.getDay()];
+      // dayCheck.textContent = dayName;
+      // console.log(dayCheck);
+      tideEntry2.textContent = (dayName);
+      tideEntry.textContent = (changeTimeZone(tideValues[i].time) + " | " + (tideValues[i].value));
     }
     console.log(tideEntry);
+    article.appendChild(tideEntry2);
     article.appendChild(tideEntry);
   }
 }
