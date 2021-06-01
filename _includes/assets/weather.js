93ebc41421445603d4e8f998f82ed889e3a2dd9c
STEVTN1 <STEVTN1@student.op.ac.nz>
2021-06-01 14:44:09 +1200

    Get weather API working

--- _includes/assets/weather.js
+++ _includes/assets/weather.js
@@ -1,11 +1,27 @@
 let latitude = "-45.878761";
 let longitude = "170.502792";
 
-let url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=5eca9eed310b8daeeeb72a1a217c3161"
+let weekdays = new Array(7);      // Array associating names with days.
+weekdays[0] = "Sunday";
+weekdays[1] = "Monday";
+weekdays[2] = "Tuesday";
+weekdays[3] = "Wednesday";
+weekdays[4] = "Thursday";
+weekdays[5] = "Friday";
+weekdays[6] = "Saturday";
+
+let url = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&appid=5eca9eed310b8daeeeb72a1a217c3161"
 fetch(url, {
     method: "GET"
 })
 .then(response => {return response.json()})
 .then(data => {
     console.log(data)
+    processWeatherData(data);
 })
+
+function processWeatherData(data) {
+    const article = document.querySelector('article')
+    const weatherValues = (data.daily);
+    console.log(weatherValues);
+}
