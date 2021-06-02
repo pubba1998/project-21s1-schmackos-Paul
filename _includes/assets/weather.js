56b5ed9fdf94e25926b816928fd92e5edcfcc20f
STEVTN1 <STEVTN1@student.op.ac.nz>
2021-06-03 10:39:23 +1200

    Display weather description and wind data

--- _includes/assets/weather.js
+++ _includes/assets/weather.js
@@ -14,11 +14,44 @@ function processWeatherData(data) {
     const article = document.querySelector('article')
     const weatherValues = (data.daily);
     const weatherEntry = document.createElement('p')
+    const weatherDescription = document.createElement('p');
+    const windEntry = document.createElement('p')
     weatherEntry.textContent ="Today's temperatures: "+noKelvin(weatherValues[0].temp.min)+" minimum, "+noKelvin(weatherValues[0].temp.max)+" maximum.";
+    weatherDescription.textContent = weatherValues[0].weather[0].description
+    windEntry.textContent = "Current windspeed: "+weatherValues[0].wind_speed.toFixed(1)+", wind direction: "+windDirection(weatherValues[0].wind_deg)
     article.appendChild(weatherEntry)
+    article.appendChild(weatherDescription)
+    article.appendChild(windEntry)
     console.log(weatherValues);
 }
 
 function noKelvin(data) {
     return(data-273.15).toFixed(1)
+}
+
+function windDirection(data) {
+    if(data > 337.5 || data <= 22.5) {
+        return "North";
+    }
+    if(data > 22.5 && data <= 67.5) {
+        return "North East";
+    }
+    if(data > 67.5 && data <= 112.5) {
+        return "East"
+    }
+    if(data > 112.5 && data <= 157.5) {
+        return "South East"
+    }
+    if(data > 157.5 && data <= 202.5) {
+        return "South"
+    }
+    if(data > 202.5 && data <= 247.5) {
+        return "South West"
+    }
+    if(data > 247.5 && data <= 292.5) {
+        return "West"
+    }
+    if(data > 292.5 && data <= 337.5) {
+        return "North West"
+    }
 }
