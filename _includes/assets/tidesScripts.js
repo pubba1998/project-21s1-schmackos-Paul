5eb62afdb601c6f5042834d8e6bf11a12a3150e3
Keru-A <alice.s.dowle@gmail.com>
2021-06-08 13:30:11 +1200

    Fix formatting of tide output

--- _includes/assets/tidesScripts.js
+++ _includes/assets/tidesScripts.js
@@ -25,7 +25,15 @@ fetch(url, {
 
 function changeTimeZone(time){      // Function changes API output to match that of the device it is on.
   let date = new Date(time);
-  return date.toLocaleString();
+  return date;
+}
+
+function formatDate(day){     // Generates the format for the date and time.
+  let day2 = day.getDate();
+  let month = (day.getMonth() + 1);
+  let hours = day.getHours();
+  let minutes = day.getMinutes();
+  return (day2 + '/'+ month + '  | ' + hours + ':' + minutes);
 }
 
 function processTideData(data){     // Extractes the tides data form the fetch quest.
@@ -37,6 +45,11 @@ function processTideData(data){     // Extractes the tides data form the fetch q
   for (let i = 0; i < tideValues.length; i++){        // for loop runs through all the data entries form the fetch and prints them out to article.
     const tideEntry = document.createElement('p');
     const tideEntry2 = document.createElement('h3');
+
+    let day = new Date(changeTimeZone(tideValues[i].time));
+      let dayName = weekdays[day.getDay()];
+      let current = new Date();
+
     if(tideValues[i].value < 1){      // if statment only prints them out if the value is less that 1 (aka low tide).
       let day = new Date(changeTimeZone(tideValues[i].time));
       let dayName = weekdays[day.getDay()];
@@ -48,8 +61,9 @@ function processTideData(data){     // Extractes the tides data form the fetch q
         article.appendChild(tideEntry2);
       };
 
-      tideEntry.textContent = (changeTimeZone(tideValues[i].time) + " | " + (tideValues[i].value));
-      article.appendChild(tideEntry);
+      tideEntry.textContent = (formatDate(day) + " | " + (tideValues[i].value));      // Fills tideEntry with data.
+          article.appendChild(tideEntry);
+          console.log(tideEntry);
     }
     console.log(tideEntry);
     
